bd46a223d2b799379946062f6151b0be674f93cc
mumertrade8@gmail.com <mumertrade8@gmail.com>
2025-01-14 06:15:47 +0000

    Updates done on cloud

--- Backend/local.py
+++ Backend/local.py
@@ -2,23 +2,49 @@ from dotenv import load_dotenv
 import pandas as pd
 import psycopg2
 import json
+import fsspec
 import os
+import s3fs
+import boto3
 
 load_dotenv()
 
-    
 def retrieve_data_from_s3():
-    # Read the JSON file
-    file_path = os.getenv("CSV_FILE_PATH")
+    #a session using the instance's role (boto3 automatically uses instance metadata credentials)
+    session = boto3.Session()
+
+    # Create an S3 client using the session
+    s3 = session.client('s3')
+
+    # Get the S3 path from environment variable
+    s3_path = os.getenv("CSV_FILE_PATH")
+
+    # Extract bucket name and key from the S3 path (e.g., s3://bucket-name/path/to/file.csv)
+    bucket_name = s3_path.split('/')[2]
+    object_key = '/'.join(s3_path.split('/')[3:])
+
+    # Read the content of the file from S3
+    response = s3.get_object(Bucket=bucket_name, Key=object_key)
+    content = response['Body'].read().decode('utf-8')
+
+    # Print the content of the file
+    #print("hahah",content)
+    return content
+
+def retrieve_data_from_s3_2():
     data = ''
-    with open(file_path, "r") as file:
-        data = json.load(file)
+    url = os.getenv("CSV_FILE_PATH")
+        
+    with fsspec.open(url, mode="r") as f:
+        # Load JSON data
+        data = json.load(f)
     return data
 
 def check_file():
     file_path = os.getenv("CSV_FILE_PATH")
+    print(file_path)
     df = pd.read_csv(file_path)
-    print("check_file: ",df.heads())
+    print("check_file: ",df)
 
 def create_db():
     try:
@@ -94,6 +120,6 @@ def input_to_response(question):
         print("Error while retrieving data from DB", e)
         return "Error while retrieving data from DB"
 
-
-# create_db()    
+print(retrieve_data_from_s3())
+#create_db()    
 input_to_response("are you familiar with python")
